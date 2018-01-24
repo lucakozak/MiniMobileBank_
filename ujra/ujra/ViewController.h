7a45b3a7e1d60155240896de0f0eee2b786e7c79
lucakozak <lucakozak@icloud.com>
2018-01-24 13:10:28 +0100

    First screen

---
+++ ujra/ujra/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ujra
+//
+//  Created by Kozak, Luca on 2018. 01. 16..
+//  Copyright © 2018. Kozak, Luca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
