9a1abbe86d782bc7eae542866f7736a388129cb4
lucakozak <lucakozak@icloud.com>
2018-01-24 13:40:21 +0100

    Other things

---
+++ ujra/ujra/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ujra
+//
+//  Created by Kozak, Luca on 2018. 01. 16..
+//  Copyright © 2018. Kozak, Luca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
